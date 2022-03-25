0dfd477a3f21491370209e53775552f03c5eccb7
annevanderaar <anne.vanderaar@student.hu.nl>
2022-03-25 22:42:38 +0100

    Added working detail call

---
+++ Library/Details.php
@@ -0,0 +1,21 @@
+<?php
+require_once("Config.php");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $url = $data['url'];
+    $id = $data['id'];
+
+    $ch = curl_init($BASE_URL . $url . $id . '?' . $API_KEY);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    if(curl_error($ch)) {
+        return curl_error($ch);
+    }
+    curl_close($ch);
+
+    echo $data;
+    // return $data;
+}
--- Library/Movies.php
+++ Library/Movies.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../Config.php");
+require_once("Config.php");
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = json_decode(file_get_contents('php://input'), true);
