8b2c47dd7695ae1ab3a20c01dcf09380e86fc520
annevanderaar <anne.vanderaar@student.hu.nl>
2022-03-26 14:24:05 +0100

    Create Discover.php

---
+++ Library/Discover.php
@@ -0,0 +1,22 @@
+<?php
+require_once("Config.php");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $page = $data['page'];
+    $url = $data['url'];
+    $genres = $data['genres'];
+
+    $ch = curl_init($BASE_URL . $url . $API_KEY . "&page=" . $page . '&with_genres=' . $genres);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec($ch);
+    if(curl_error($ch)) {
+        return curl_error($ch);
+    }
+    curl_close($ch);
+
+    echo $data;
+    // return $data;
+}
