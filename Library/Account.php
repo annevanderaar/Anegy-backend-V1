d5576df6908903cc2556e55d736fd6d217c57bb6
annevdaar <anne@snelwebcenter.nl>
2022-05-11 14:45:27 +0100

    added sql file and login request

--- Library/Account.php
+++ Library/Account.php
@@ -2,11 +2,16 @@
 require_once("DB/dbh.php");
 require_once("Config.php");
 
-if ($_SERVER["REQUEST_METHOD"] === "GET") { 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $email = $data['email'];
+    $password = $data['password'];
+    echo $email;
+} else if ($_SERVER["REQUEST_METHOD"] === "GET") {
     $db = new dbconnection();
     $id = 1;
     $output = $db->getUser($id);
     // $output = json_decode($output);
     $output = json_encode($output);
-    echo($output);
+    echo ($output);
 }
