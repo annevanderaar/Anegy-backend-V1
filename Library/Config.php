a24e4dd00722de99bf6d6eba2bc0adad5a857768
Anne van der Aar <74538510+annevanderaar@users.noreply.github.com>
2022-06-02 13:53:37 +0200

    update

--- Library/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$API_KEY = "";
-$BASE_URL = "https://api.themoviedb.org/3";
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
